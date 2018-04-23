a6ff13d25ddffaf5a4d567123d8e13a11bc4b60d
Gianluigi Zanetti <zag@crs4.it>
2018-04-23 12:54:17 +0200

    Added formally complete implementation of tflow.Retrainer

--- tflow.py
+++ tflow.py
@@ -1,26 +1,22 @@
-"""
-The contents of this file have been plagiarized, with abandon,
-from [tensorflow for poets](FIXME-link).
+# The contents of this file have been plagiarized, with abandon,
+# from [tensorflow for poets](FIXME-link).
 
-The strategy supported by this module is the following:
+# The strategy supported by this module is the following:
 
- 1. create all the specialized graphs needed for:
-    1. bottleneck calculation
-    2. new traning network
- 2. save the new graphs to hdfs
- 3. use specialized classes to load and execute the graphs.
+#  1. create all the specialized graphs needed for:
+#     1. bottleneck calculation
+#     2. new traning network
+#  2. save the new graphs to hdfs
+#  3. use specialized classes to load and execute the graphs.
 
-model structure
-
-"""
 
 # First pass implementation, we are not using any of the modern stuff like
 # the tf.data API
 
-
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 import numpy as np
+import datetime
 
 
 def load_graph(path, return_elements):
@@ -111,35 +107,39 @@ def variable_summaries(var):
 
 class Retrainer(object):
     @classmethod
-    def create_graph(cls, model, class_count):
-        """Adds a new softmax and fully-connected layer for training"""
+    def create_graph(cls, model, n_classes):
+        """FIXME"""
         with tf.Graph().as_default() as graph:
             (ground_truth_input,
-             final_tensor) = cls.add_training_ops(model, class_count)
+             final_tensor) = cls.add_training_ops(model, n_classes)
             cls.add_evaluation_step(final_tensor, ground_truth_input)
         return graph
 
     @classmethod
-    def add_training_ops(cls, model, class_count):
-        """foo """
-        bneck_size = model['bottel_neck_tensor_size']
+    def add_training_ops(cls, model, n_classes):
+        """Adds a training graph.
+
+        The graph will accept in input a bottleneck vector, with size
+        defined in model, and a ground truth with n_classes possible
+        options."""
+        bneck_size = model['bottleneck_tensor_size']
         bneck_input = tf.placeholder(
-            tf.float32, shape=[None, bneck_size], name='bneck_input')
+            tf.float32, shape=[None, bneck_size], name='bottleneck_input')
         ground_truth_input = tf.placeholder(
-            tf.float32, [None, class_count], name='ground_truth_input')
+            tf.float32, [None, n_classes], name='ground_truth_input')
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
-        # Organizing the following ops as `final_training_ops`
+        # Organizing the following ops as `training_ops`
         # so they're easier to see in TensorBoard
-        layer_name = 'final_training_ops'
+        layer_name = 'training_ops'
         with tf.name_scope(layer_name):
             with tf.name_scope('weights'):
                 initial_value = tf.truncated_normal(
-                    [bneck_size, class_count], stddev=0.001)
+                    [bneck_size, n_classes], stddev=0.001)
                 layer_weights = tf.Variable(initial_value,
                                             name='final_weights')
                 variable_summaries(layer_weights)
             with tf.name_scope('biases'):
-                layer_biases = tf.Variable(tf.zeros([class_count]),
+                layer_biases = tf.Variable(tf.zeros([n_classes]),
                                            name='final_biases')
                 variable_summaries(layer_biases)
             with tf.name_scope('Wx_plus_b'):
@@ -149,7 +149,7 @@ class Retrainer(object):
         tf.summary.histogram('activations', final_tensor)
         with tf.name_scope('cross_entropy'):
             cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
-                            labels=ground_truth_input, logits=logits)
+                labels=ground_truth_input, logits=logits)
             with tf.name_scope('total'):
                 cross_entropy_mean = tf.reduce_mean(cross_entropy)
         tf.summary.scalar('cross_entropy', cross_entropy_mean)
@@ -165,20 +165,126 @@ class Retrainer(object):
         """
         with tf.name_scope('accuracy'):
             with tf.name_scope('correct_prediction'):
-                prediction = tf.argmax(result_tensor, 1)
+                prediction = tf.argmax(result_tensor, 1, name="prediction")
                 correct_prediction = tf.equal(
                     prediction, tf.argmax(ground_truth_tensor, 1))
             with tf.name_scope('accuracy'):
                 evaluation_step = tf.reduce_mean(
-                    tf.cast(correct_prediction, tf.float32))
+                    tf.cast(correct_prediction, tf.float32),
+                    name="evaluation_step")
         tf.summary.scalar('accuracy', evaluation_step)
 
     def __init__(self, path):
-        self.graph = load_graph(path)
         # we exploit the fact that we know what the graph should look like,
         # since it was built with our class method. Therefore, we can directly
         # extract the tensors we need.
+        graph, (bottleneck_input, ground_truth_input,
+                learning_rate, train_step, cross_entropy, final_tensor,
+                evaluation_step, prediction) = \
+            load_graph(path, ['bottleneck_input:0', 'ground_truth_input:0',
+                              'learning_rate:0', 'train_step:0',
+                              'cross_entropy:0', 'final_tensor:0',
+                              'evaluation_step:0', 'prediction:0'])
+        self.graph = graph
+        self.bottleneck_input = bottleneck_input
+        self.ground_truth_input = ground_truth_input
+        self.learning_rate = learning_rate
+        self.train_step = train_step
+        self.cross_entropy = cross_entropy
+        self.final_tensor = final_tensor
+        self.evaluation_step = evaluation_step
+        self.prediction = prediction
 
-    def run(self, xxx):
-        # here we actually run the calculation
-        pass
+    def run_training_step(self, s, i, merged, bottlenecks, ground_truths):
+        # Feed the bottlenecks and ground truth into the graph, and run a
+        # training step. Capture training summaries for TensorBoard with
+        # the `merged` op.
+        summary, _ = s.run(
+            [merged, self.train_step],
+            feed_dict={self.bottleneck_input: bottlenecks,
+                       self.ground_truth_input: ground_truths})
+        return summary
+
+    def run_check_training(self, s, i, bottlenecks, ground_truths):
+        accuracy, cross_entropy_value = s.run(
+            [self.evaluation_step, self.cross_entropy],
+            feed_dict={self.bottleneck_input: bottlenecks,
+                       self.ground_truth_input: ground_truths})
+        tf.logging.info('%s: Step %d: Train accuracy = %.1f%%' %
+                        (datetime.now(), i, accuracy * 100))
+        tf.logging.info('%s: Step %d: Cross entropy = %f' %
+                        (datetime.now(), i, cross_entropy_value))
+
+    def run_validation(self, s, i, bottlenecks, ground_truths):
+        summary, accuracy = s.run(
+            [self.merged, self.evaluation_step],
+            feed_dict={self.bottleneck_input: bottlenecks,
+                       self.ground_truth_input: ground_truths})
+        tf.logging.info(
+            '%s: Step %d: Validation accuracy = %.1f%% (N=%d)' %
+            (datetime.now(), i, accuracy * 100, len(bottlenecks)))
+        return summary
+
+    def run_test(self, s, bottlenecks, ground_truths, filenames,
+                 do_print=False):
+        accuracy, predictions = s.run(
+            [self.evaluation_step, self.prediction],
+            feed_dict={self.bottleneck_input: bottlenecks,
+                       self.ground_truth_input: ground_truths})
+        tf.logging.info('Final test accuracy = %.1f%% (N=%d)' %
+                        (accuracy * 100, len(bottlenecks)))
+        if do_print:
+            tf.logging.info('=== MISCLASSIFIED TEST IMAGES ===')
+            for i, filename in enumerate(filenames):
+                if predictions[i] != ground_truths[i].argmax():
+                    tf.logging.info('%70s  is %s predicts %s' %
+                                    (filename, ground_truths[i].argmax(),
+                                     predictions[i]))
+
+    def run(self, FLAGS, get_random_bottlenecks):
+        """run the training
+
+        get_random_bottlenecks is a function that requires in input the
+        following parameters:
+
+         - category: a string choosen between training, testing or validation
+         - how_many: an integer that specifies how large should be the random
+                     sampling, if negative returns all
+        returns:
+
+        - three lists with, respectively, bottleneck arrays, ground
+          truths, and images filenames.
+        """
+        with tf.Session(graph=self.graph) as s:
+            merged = tf.summary.merge_all()
+            train_writer = tf.summary.FileWriter(
+                FLAGS.summaries_dir + '/train', s.graph)
+            validation_writer = tf.summary.FileWriter(
+                FLAGS.summaries_dir + '/validation')
+            init = tf.global_variables_initializer()
+            s.run(init)
+            for i in range(FLAGS.how_many_training_steps):
+                (train_bottlenecks,
+                 train_ground_truths, _) = get_random_bottlenecks(
+                     'training', FLAGS.train_batch_size)
+                train_summary = self.run_training_step(
+                    s, i, merged, train_bottlenecks, train_ground_truths)
+                train_writer.add_summary(train_summary, i)
+                # check from time to time
+                is_last_step = (i + 1 == FLAGS.how_many_training_steps)
+                if (i % FLAGS.eval_step_interval) == 0 or is_last_step:
+                    self.run_check_training(s, i, train_bottlenecks,
+                                            train_ground_truths)
+                    (validation_bottlenecks, validation_ground_truth, _
+                     ) = get_random_bottlenecks(
+                         'validation', FLAGS.validation_batch_size)
+                    validation_summary = self.run_validation(
+                        s, i, validation_bottlenecks, validation_ground_truth)
+                    validation_writer.add_summary(validation_summary, i)
+
+            # final test
+            (test_bottlenecks, test_ground_truth, test_filenames
+             ) = get_random_bottlenecks('testing', FLAGS.test_batch_size)
+            self.run_test(s, test_bottlenecks, test_ground_truth,
+                          test_filenames,
+                          FLAGS.print_misclassified_test_images)
