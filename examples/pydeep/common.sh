0063d1d1971e2d870c7c3d530e31ba06711b8a8a
simleo <simleo@crs4.it>
2018-07-06 16:16:59 +0200

    pydeep: allow to set RNG seed

--- examples/pydeep/common.sh
+++ examples/pydeep/common.sh
@@ -14,4 +14,25 @@ downsample () {
     [ -n ${reset_x} ] && set -x
 }
 
-export -f use_two_classes downsample
+utc_timestamp_ns () {
+    date +%s%N
+}
+
+export -f use_two_classes downsample utc_timestamp_ns
+
+export BNECKS_DIR="${BNECKS_DIR:-bottlenecks}"
+export GENBNECKS_NUM_MAPS="${GENBNECKS_NUM_MAPS-4}"
+export LIBHDFS_OPTS="${LIBHDFS_OPTS:--Xmx512m}"
+export NUM_STEPS="${NUM_STEPS:-400}"
+export PLOTS_DIR="${PLOTS_DIR:-training_plots}"
+export RETRAIN_NUM_MAPS="${RETRAIN_NUM_MAPS-4}"
+export TEST_NUM_MAPS="${TEST_NUM_MAPS-2}"
+export TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-test_results}"
+export TRAINED_MODELS_DIR="${TRAINED_MODELS_DIR:-trained_models}"
+
+if [ -n "${DEBUG:-}" ]; then
+    LOG_LEVEL="DEBUG"
+else
+    LOG_LEVEL="INFO"
+fi
+export LOG_LEVEL
